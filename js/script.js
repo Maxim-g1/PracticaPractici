7fe0c77a5ede30c1c7fd2538bb62c2888243ad53
Maxim-g1 <122662234+Maxim-g1@users.noreply.github.com>
2024-03-15 17:59:27 +0500

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -3,11 +3,20 @@ const menu = document.querySelector('.menu')
 const tabs = document.querySelectorAll('.tab')
 const tab = document.querySelector('.tab')
 
-menu.addEventListener('click', (e) => {
-    // выведем в консоль значение дата атрибута кнопки по которой кликнули
-    for (let taball of tab) {
-        taball.classList.remove('active')
-    }
 
+
+menu.addEventListener('click', (e) => {
+    let targetBtn = e.target
+    let dataText = targetBtn.dataset.text
+    for (let tabAll of tabs) {
+        tabAll.classList.remove('active')
+
+        if (dataText === tabAll.id) {
+
+            
+            tabAll.classList.add('active')
+            targetBtn.classList.add('active')
+        }
+    }
 
 })
